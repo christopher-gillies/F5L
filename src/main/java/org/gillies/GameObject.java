425b1311a97de6db5a4f270272b58107a7f6f6d7
cgillies@umich.edu <cgillies@umich.edu>
2016-02-06 14:48:19 -0500

    started training

--- src/main/java/org/gillies/GameObject.java
+++ src/main/java/org/gillies/GameObject.java
@@ -5,26 +5,38 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.kidneyomics.hmm.HMM;
+import org.kidneyomics.hmm.State;
 import org.kidneyomics.hmm.Symbol;
+import org.kidneyomics.hmm.HMM.LEARN_MODE;
 
 public class GameObject {
 	
 	private final Map<String,List<Symbol>> map;
+	private final Map<String,List<Symbol>> train;
+	private final Map<String,List<Symbol>> test;
+	
+	private final Map<String,HMM> hmms;
+	final Symbol played;
+	final Symbol notPlayed;
 	
 	private GameObject() {
+		train = new HashMap<String, List<Symbol>>();
+		test = new HashMap<String, List<Symbol>>();
 		map = new HashMap<String, List<Symbol>>();
+		hmms = new HashMap<String, HMM>();
+		played = Symbol.createSymbol("P");
+		notPlayed = Symbol.createSymbol("N");
 	}
 	
 	public static GameObject getGameObject(List<DrawingResult> in, int maxNumber) {
 		
-		Symbol played = Symbol.createSymbol("P");
-		Symbol notPlayed = Symbol.createSymbol("N");
 		GameObject go = new GameObject();
 		
 		
 		
 		for(DrawingResult result : in) {
-			Map<String,Symbol> vector = result.toVector(maxNumber, played, notPlayed);
+			Map<String,Symbol> vector = result.toVector(maxNumber, go.played, go.notPlayed);
 			
 			for(Map.Entry<String, Symbol> entry : vector.entrySet()) {
 				if(go.map.containsKey(entry.getKey())) {
@@ -39,11 +51,83 @@ public class GameObject {
 		}
 		
 		
-		
+		//buildHMMS
+		go.buildHMMs();
 		return go;
+	}
+	
+	public void train(int numTrain) {
+		for(String num : map.keySet()) {
+			System.err.println("Training: " + num);
+			HMM hmm = hmms.get(num);
+			List<Symbol> symbols = map.get(num);
+			List<Symbol> trainSyms = new LinkedList<Symbol>();
+			List<Symbol> testSyms = new LinkedList<Symbol>();
+			
+			train.put(num, trainSyms);
+			test.put(num, testSyms);
+			
+			int count = 0;
+			for(Symbol symbol : symbols) {
+				if(count > numTrain) {
+					testSyms.add(symbol);
+				} else {
+					trainSyms.add(symbol);
+				}
+			}
+			
+			hmm.learnEMSingle(trainSyms, LEARN_MODE.CUSTOM);
+		}
+	}
+	
+	private void buildHMMs() {
+		for(String num : map.keySet()) {
+			HMM hmm = buildHMM();
+			this.hmms.put(num, hmm);
+		}
+	}
+	
+	private HMM buildHMM() {
+		State start = State.createStartState();
+		
+		
+		State fair = State.createState("F");
+		//set emission probabilities for fair state
+		fair.getEmissions().setProbability(played, 0.5);
+		fair.getEmissions().setProbability(notPlayed, 0.5);
+		
+		
+		
+		State biased = State.createState("B");
+		//set emission probabilities for biased state
+		biased.getEmissions().setProbability(played, 0.5);
+		biased.getEmissions().setProbability(notPlayed, 0.5);
+		
+		//equal chance to be in fair or biased state
+		start.getTransitions().setProbability(fair, 0.5);
+		start.getTransitions().setProbability(biased, 0.5);
+		
+		fair.getTransitions().setProbability(fair, 0.5);
+		fair.getTransitions().setProbability(biased, 0.1);
+		
+		biased.getTransitions().setProbability(fair, 0.5);
+		biased.getTransitions().setProbability(biased, 0.5);
+		
+		HMM hmm = HMM.createHMMFromStartState(start);
+		
+		hmm.initializeStateCounts(LEARN_MODE.PSEUDO_COUNT);
+		fair.getTransitions().addToCount(fair, 1);
+		biased.getTransitions().addToCount(biased, 1);
+		
+		biased.getEmissions().addToCount(notPlayed, 1);
+		return hmm;
 	}
 	
 	public Map<String, List<Symbol>> getMap() {
 		return this.map;
+	}
+	
+	public Map<String,HMM> getHMMs() {
+		return this.hmms;
 	}
 }
--- src/test/java/org/gillies/GameObjectTest.java
+++ src/test/java/org/gillies/GameObjectTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
+import org.kidneyomics.hmm.HMM;
 import org.kidneyomics.hmm.Symbol;
 
 public class GameObjectTest {
@@ -24,14 +26,37 @@ public class GameObjectTest {
 		GameObject go = GameObject.getGameObject(results, 39);
 		Map<String,List<Symbol>> map = go.getMap();
 		
+		Map<String,HMM> hmms = go.getHMMs();
+		
+		
 		int count = 0;
 		for(Map.Entry<String, List<Symbol>> entry : map.entrySet()) {
 			//System.err.println(entry.getKey());
 			//System.err.println(entry.getValue().size());
 			assertEquals(4154,entry.getValue().size());
 			count++;
+			
+			HMM hmm = hmms.get(entry.getKey());
+			assertNotNull(hmm);
+			
 		}
 		
 		assertEquals(39,count);
+		
+		
+		go.train(3000);
+		
+		LinkedList<Symbol> sym = new LinkedList<Symbol>();
+		sym.add(go.played);
+		
+		for(Map.Entry<String, HMM> entry : hmms.entrySet()) {
+			System.out.println(entry.getKey());
+
+			double probOfP = entry.getValue().evaluate(sym, false);
+			
+			System.out.println("Prob of play: " + probOfP);
+			entry.getValue().printProbabilities();
+		}
+		
 	}
 }
